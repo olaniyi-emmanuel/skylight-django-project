a4cd896e4fda7763b29b4f71f6a9277050ce97e7
emmanuel-olafusi <emmanuelolafusi.digifyng@gmail.com>
2023-08-10 17:43:05 +0100

    Started the project off with frontend out put plus some MVT done

---
+++ skylight/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class profile (models.Model):
+    user = pass
+    id_user = 
+    bio = 
+    profileimg = models.ImageField(upload_to='profile_images')
+    location = 
